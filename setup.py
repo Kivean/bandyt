c328ce381d7b2c30db4ab453573b55afda11ed5c
bandyt group <bandytgroup@gmail.com>
2024-09-26 19:54:09 -0700

    Updated __init__.py to import modules and functions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bandyt',                   # Your module's name
-    version='0.5',                      # Initial release version
+    version='0.6',                      # Initial release version
     packages=['bandyt'],           # Automatically find packages in your project
     install_requires=[                  # Dependencies
         'numpy',
